5d96fc400443faf199df25dbc7aa57e076e8898a
Jovan Tanevski <tanevski@gmail.com>
2019-03-20 14:36:25 +0100

    preliminary feature importance estimation pipeline for melanoma

--- melanoma_pipeline.R
+++ melanoma_pipeline.R
@@ -1,17 +1,22 @@
-library(tidyverse)
-library(distances)
-library(furrr)
+library(FNN)
+source("multiview.R")
 
-m1 <- read_csv("data/melanoma/33466POST.csv", col_types = cols())
+plan(multiprocess, workers=15)
+
+m1 <- read_csv("data/melanoma/33466/33466POST.csv", col_types = cols())
 
 intensities <- m1 %>% select(starts_with("Cell"))
+#negative values (-Inf) in Hoechst2, pERK, AXL and MITF
+intensities <- as.data.frame(apply(intensities,2,function(i) i <- pmax(0,i)))
 positions <- m1 %>% select(CellId, contains("position"))
 
-#The percent_touching neighbors with the smallest disntance to the cell are actually touching
+write_delim(intensities[,-1],"data/melanoma/33466/expressions.txt", delim = " ")
+write_csv(positions[,-1],"data/melanoma/33466/positions.txt")
+
 neighborhood <- m1 %>% select(CellId, Percent_Touching, contains("neighbo",ignore.case = T))
 stats <- m1 %>% select(CellId, 43:51)
 
-plan(multiprocess, workers=4)
+
 
 dists <- distances(as.data.frame(positions[,-1]))
 l <- (2*400)^2
@@ -25,5 +30,41 @@ t1 <- Sys.time()
 
 print(t1-t0)
 
-saveRDS(para.view, "para.view.Rds")
-                                              
+colnames(para.view) <- paste0("p",colnames(para.view))
+write_rds(para.view, paste0("para.view.",l,".rds"))
+
+
+#generate juxta from neighborhood information
+
+t0 <- Sys.time()
+juxta.view <- seq(nrow(expr)) %>% future_map_dfr(function(cellid){
+  all.neighbors <- neighborhood %>% slice(cellid) %>% select(starts_with("neighbour")) %>% unlist
+  all.neighbors <- all.neighbors[all.neighbors > 0]
+  
+  #maybe
+  #The percent_touching neighbors with the smallest disntance to the cell are actually touching
+  #percent.touching <- neighborhood %>% slice(cellid) %>% pull(Percent_Touching)
+  if(length(all.neighbors) == 0){
+    juxta.cell <- (expr %>% slice(1)) * 0 %>% t
+  } else {
+    juxta.cell <- expr %>% slice(all.neighbors) %>% colSums %>%  t
+  }
+  
+  data.frame(juxta.cell)
+}, .progress = TRUE)
+t1 <- Sys.time()                   
+
+print(t1-t0)
+
+colnames(juxta.view) <- paste0("j",colnames(juxta.view))
+write_rds(juxta.view, "juxta.view.rds")
+
+
+data <- list.dirs("./data/melanoma", recursive=FALSE)
+
+l <- (2*400)^2
+calculateImportances(data,data.l=l,neighbor.thr = NA)
+
+#for comparison
+corrplot::corrplot(cor(expr),type = "upper", diag = F)
+
