d158d1efed31a788bde0de5ec547d2ef6b2ded32
Ricardo Ramírez <rramirez@lcg.unam.mx>
2020-12-17 13:41:01 +0100

    refactored code, followed package conventions

--- R/pipelines.R
+++ R/pipelines.R
@@ -1,9 +1,8 @@
-# Copyright (c) [2020] [Ricardo O. Ramirez Flores, Jovan Tanevski]
+# Copyright (c) [2020] [Ricardo O. Ramirez Flores]
 # roramirezf@uni-heidelberg.de
-#
-# Collection of wrapper functions to build automatic pipelines
-# Suggest a Seurat wrapper for Visium data
 
+#' Defines general misty pipelines for 
+#' their usage with Seurat objects 10x
 
 #' Builds automatic MISTy pipelines for 10x Visium data in Seurat format
 #' 
@@ -19,38 +18,34 @@
 #' the l parameter of the paraview or the number of neighbours used in the juxtaview
 #' @param spot_ids: spot IDs to fit MISTy if null all spots are used
 #' @param out_alias: folder name to be used in all MISTy outputs
-MISTy_seurat = function(visium_slide,
-                        view_assays,
-                        view_features,
-                        view_types,
-                        view_params,
-                        spot_ids = NULL,
-                        out_alias = "default",
-                        workers = 4){
+run_misty_seurat <- function(visium.slide,
+                         view.assays,
+                         view.features,
+                         view.types,
+                         view.params,
+                         spot.ids = NULL,
+                         out.alias = "default"){
   
-  # Geometry extraction
-  geometry = visium_slide@images$slice1@coordinates[,c(2,3)]
+  # Geometry extraction --------------------------------  
+  geometry <- visium_slide@images$slice1@coordinates[,c(2,3)]
   
-  # Extracting data
-  view_data = map(view_assays, 
-                  extract_seurat_data, 
-                  geometry = geometry,
-                  visium_slide = visium_slide)
+  # Extracting data -------------------------------- 
+  view.data <- map(view.assays, 
+                   extract_seurat_data, 
+                   geometry = geometry,
+                   visium.slide = visium.slide)
   
-  # Building pipeline
-  
-  build_MISTy_pipeline(view_data = view_data,
-                       view_features = view_features,
-                       view_types = view_types,
-                       view_params = view_params,
+  # Building pipeline --------------------------------
+  build_misty_pipeline(view.data = view.data,
+                       view.features = view.features,
+                       view.types = view.types,
+                       view.params = view.params,
                        geometry = geometry,
-                       spot_ids = spot_ids,
-                       out_alias = out_alias,
-                       workers = workers)
-  
+                       spot.ids = spot.ids,
+                       out.alias = out.alias)
 }
 
-#' Builds complete MISTy pipelines
+#' Builds automatic MISTy pipelines 
 #' 
 #' Warning: Feature IDs can't have "-" symbol
 #' Warning: rownames must be the same in intra_df, para_df and geometry
@@ -63,50 +58,42 @@ MISTy_seurat = function(visium_slide,
 #' @param geometry: a data frame with IDs as rows two coordinates as columns 
 #' @param spot_ids: spot IDs to fit MISTy if null all rows are used
 #' @param out_alias: folder name to be used in all MISTy outputs
-build_MISTy_pipeline = function(view_data,
-                                view_features,
-                                view_types,
-                                view_params,
-                                geometry,
-                                spot_ids = NULL,
-                                out_alias = "default",
-                                workers = 4){
+build_misty_pipeline <- function(view.data,
+                                 view.features,
+                                 view.types,
+                                 view.params,
+                                 geometry,
+                                 spot.ids = NULL,
+                                 out.alias = "default"){
   
-  plan(multiprocess, workers = workers)
-  
-  clear_cache()
-  
-  #Checking areas to consider
-  if(is.null(spot_ids)){
-    spot_ids = rownames(view_data[[1]])
+  # Adding all spots ids in case they are not defined ----------
+  if(is.null(spot.ids) == TRUE){
+    spot.ids <- rownames(view.data[[1]])
   }
   
-  #First filter the features from the data
-  view_data = map2(view_data,view_features,filter_data_features)
+  # First filter the features from the data ------------
+  view.data.filt <- map2(view.data, view.features, filter_data_features)
   
-  #Create initial view
-  #Defining useful data intra
-  views_main = create_initial_view(view_data[[1]][spot_ids,], 
+  # Create initial view ------------------------------------------
+  views.main <- create_initial_view(view.data.filt[[1]][spot.ids, ], 
                                    unique.id = "main")
   
-  #Create other views
-  all_views = pmap(list(view_data[-1],
-                        view_types[-1],
-                        view_params[-1],
-                        names(view_data[-1])), 
-                   create_all_views, 
-                   spot_ids = spot_ids,
-                   geometry = geometry)
+  # Create other views -------------------------------------------
+  view.names = names(view.data.filt)
   
+  all.views <- pmap(list(view.data.filt[-1],
+                         view.types[-1],
+                         view.params[-1],
+                         view.names[-1]), 
+                    create_any_view, 
+                    spot.ids = spot.ids,
+                    geometry = geometry)
   
-  # Define frankenstein view
-  for(i in 1:length(all_views)){
-    views_main = views_main %>%
-      add_views(all_views[[i]])
-  }
-  
-  # Run MISTy
-  MISTy_run = run_misty(views_main,out_alias)
+  pline.views <- add_views(views.main, 
+                           unlist(all.views,recursive = F))
+    
+  # Run MISTy ----------------------------------
+  run_misty(pline.views,out.alias)
 }
 
 ################
@@ -115,91 +102,84 @@ build_MISTy_pipeline = function(view_data,
 
 #'Extracts data from an specific assay in a Seurat object
 #'and aligns the IDs to be identical to geometry
-extract_seurat_data = function(visium_slide,assay,geometry){
-  data_df = as.matrix(visium_slide@assays[[assay]]@data)
-  data_df = data_df %>%
+extract_seurat_data <- function(visium.slide,
+                                assay,
+                                geometry) {
+  data_df <- as.matrix(visium.slide@assays[[assay]]@data)
+  data_df <- data_df %>%
     t %>% data.frame(check.names = F)
-  data_df = data_df[rownames(geometry),]
+  data_df <- data_df[rownames(geometry), ]
   return(data_df)
 }
 
-#' Filters data to contain features of interested
-filter_data_features = function(data, features){
-  if(is.null(features)){
-    features = colnames(data)
+#' Filters data to contain features of interest
+filter_data_features <- function(data, 
+                                 features) {
+  if (is.null(features) == TRUE) {
+    features <- colnames(data)
   }
-  data_df = data[,features]
-  colnames(data_df) = gsub("-","_", colnames(data_df))
+  data_df <- data[, features]
+  colnames(data_df) <- gsub("-", "_", colnames(data_df))
   return(data_df)
 }
 
 #' Builds views depending on the paramaters defined
-#' @param data = data.frame containing IDs in rows and features in columns
-#' @param view_type = type of view to be generated
-#' @param view_param = l or neighbor.thr parameter
-#' @param spot_ids = area to be considered in the analysis
-#' @param geometry = location data frame containing IDs in rows and coordinates in columns 
-create_all_views = function(data, 
-                            view_type,
-                            view_param,
-                            view_name,
-                            spot_ids,
-                            geometry){
+#' @param data <- data.frame containing IDs in rows and features in columns
+#' @param view_type <- type of view to be generated
+#' @param view_param <- l or neighbor.thr parameter
+#' @param spot_ids <- area to be considered in the analysis
+#' @param geometry <- location data frame containing IDs in rows and coordinates in columns 
+create_any_view <- function(data, 
+                            view.type,
+                            view.param,
+                            view.name,
+                            spot.ids,
+                            geometry) {
   
-  if(view_type == "intra"){
+  MISTy::clear_cache()
+  
+  if (view.type == "intra") {
     
-    view_data_tmp = create_initial_view(data, unique.id = view_name)
-    
-    # Spot specific view comes from the view above
-    data_red = view_data_tmp[["intracellular"]]$data
-    data_red = data_red[spot_ids,]
-    
-  }else if(view_type == "para"){
-    
-    view_data_tmp = create_initial_view(data, unique.id = view_name) %>% 
-      add_paraview(geometry,l = view_param^2)
+    view.data.tmp <- create_initial_view(data, unique.id = view.name)
     
     # Spot specific view comes from the view above
-    data_red = view_data_tmp[[3]]$data
-    rownames(data_red) = rownames(view_data_tmp[["intracellular"]]$data) #we named rows just for easy access
-    data_red = data_red[spot_ids,]
+    data.red <- view.data.tmp[["intracellular"]]$data
+    data.red <- data.red[spot.ids, ]
     
-  }else if(view_type == "juxta"){
-    view_data_tmp = create_initial_view(data, unique.id = view_name) %>% 
-      add_juxtaview(positions = geometry,neighbor.thr = view_param)
+  } else if (view.type == "para") {
     
-    # Spot specific view comes from the view above
-    data_red = view_data_tmp[[3]]$data
-    rownames(data_red) = rownames(view_data_tmp[["intracellular"]]$data) #we named rows just for easy access
-    data_red = data_red[spot_ids,]
+    view.data.tmp <- create_initial_view(data, unique.id = view.name) %>% 
+      add_paraview(geometry,l = view.param^2)
+    
+    # Spot specific view comes from the view above -----------------------
+    data.ix <- paste0("paraview.", view.param^2)
+    data.red <- view.data.tmp[[data.ix]]$data
+    rownames(data.red) <- rownames(view.data.tmp[["intracellular"]]$data)
+    data.red <- data.red[spot.ids, ]
+    
+  }else if(view.type == "juxta"){
+    view.data.tmp <- create_initial_view(data, unique.id = view.name) %>% 
+      add_juxtaview(positions = geometry,
+                    neighbor.thr = view.param)
+    
+    # Spot specific view comes from the view above ----------------------
+    data.ix <- paste0("juxtaview.", view.param)
+    data.red <- view.data.tmp[[data.ix]]$data
+    rownames(data.red) <- rownames(view.data.tmp[["intracellular"]]$data)
+    data.red <- data.red[spot.ids, ]
     
   }
   
-  return(create_view(paste0(view_name,"_",view_param),data_red))
+  if (is.null(view.param) == TRUE) {
+    MISTy.view <- create_view(paste0(view.name), 
+                             data.red)
+  } else {
+    MISTy.view <- create_view(paste0(view.name,"_",view.param), 
+                             data.red)
+  }
   
+  return(MISTy.view)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
