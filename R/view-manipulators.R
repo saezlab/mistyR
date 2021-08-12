d8e32c3f65d59819dae3c3b824e5ee50f1bbe94b
Jovan Tanevski <tanevski@gmail.com>
2021-08-12 14:24:08 +0200

    add view filtering and marker selection

---
+++ R/view-manipulators.R
@@ -0,0 +1,130 @@
+#' Filter spatial units
+#'
+#' Select, remove (or duplicate) rows from all views in a composition by their
+#' row locations or according to conditions based on a specific view.
+#'
+#' The values in \code{rows} have priority over the other parameters. If
+#' \code{rows} doesn't contain integer values then filtering
+#' is performed based on the view specified in \code{view} and expressions
+#' (\code{\link[dplyr:dplyr_data_masking]{...}}) returning logical values
+#' defined in terms of the variables in \code{view}.
+#'
+#' @param current.views the current view composition.
+#' @param rows row (integer) location; positive values to keep (duplicate)
+#' and/or negative to remove.
+#' @param view the name of the view to be used for filtering.
+#' @param ... logical expressions defined in terms of the variables in
+#' \code{view} passed on to \code{\link[dplyr:filter]{dplyr::filter}()}.
+#'
+#' @return A mistyR view composition with filtered spatial units from all views.
+#'
+#' @seealso <\code{\link[dplyr:dplyr_data_masking]{data-masking}}>.
+#'
+#' @family view manipulation functions
+#'
+#' @examples
+#' # Create a view composition with an intraview and filter
+#'
+#' library(dplyr)
+#'
+#' # get the expression data
+#' data("synthetic")
+#' expr <- synthetic[[1]] %>% select(-c(row, col, type))
+#'
+#' # compose
+#' misty.views <- create_initial_view(expr)
+#'
+#' # select only the first 10 spatial units and preview
+#' misty.views %>%
+#'   filter_views(1:10) %>%
+#'   str()
+#'
+#' # select only the units where the expression of ligA is larger than 0.5
+#' # and preview
+#' misty.views %>%
+#'   filter_views(NA, "intraview", ligA > 0.5) %>%
+#'   str()
+#' @export
+filter_views <- function(current.views, rows, view = "intraview", ...) {
+  if (!is.numeric(rows)) {
+    assertthat::assert_that(length(view) == 1,
+      msg = "Please select only a single view for filtering."
+    )
+    assertthat::assert_that(view %in% names(current.views),
+      msg = "The requested view cannot be found in the current view composition."
+    )
+    assertthat::assert_that(view != "misty.uniqueid",
+      msg = "Filtering based on the unique id is not possible."
+    )
+
+    # check that filter expressions are logical
+    dplyr:::check_filter(dplyr:::dplyr_quosures(...))
+
+    toslice <- misty.views[[view]][["data"]] %>%
+      dplyr::transmute(...) %>%
+      apply(1, purrr::reduce, `&`) %>%
+      which()
+  } else {
+    toslice <- rows
+  }
+
+  purrr::modify_if(
+    current.views,
+    ~ length(.x) > 1,
+    ~ purrr::modify_at(.x, "data", ~ dplyr::slice(.x, toslice))
+  )
+}
+
+
+#' Select a subset of markers in a view
+#'
+#' @param current.views the current view composition.
+#' @param view the name of the view to select markers for.
+#' @param ... one or more \link[dplyr:dplyr_tidy_select]{select} expressions
+#' \code{\link[dplyr:select]{dplyr::select}()} for the specified \code{view}.
+#'
+#' @return A mistyR view composition with selected markers in \code{view}.
+#'
+#' @seealso <\code{\link[dplyr:dplyr_tidy_select()]{tidy-select}}>.
+#'
+#' @family view manipulation functions
+#'
+#' @examples
+#' # Create a view composition with an intraview and select
+#'
+#' library(dplyr)
+#'
+#' # get the expression data
+#' data("synthetic")
+#' expr <- synthetic[[1]] %>% select(-c(row, col, type))
+#'
+#' # compose
+#' misty.views <- create_initial_view(expr)
+#'
+#' # select markers from the intraview not starting with lig and preview
+#' misty.views %>%
+#'   select_markers("intraview", !starts_with("lig")) %>%
+#'   str()
+#' @export
+select_markers <- function(current.views, view = "intraview", ...) {
+  assertthat::assert_that(length(view) == 1,
+    msg = "Please select only a single view for marker selection."
+  )
+  assertthat::assert_that(view %in% names(current.views),
+    msg = "The requested view cannot be found in the current view composition."
+  )
+  assertthat::assert_that(view != "misty.uniqueid",
+    msg = "Marker selection based on the unique id is not possible."
+  )
+
+  purrr::modify_at(
+    current.views, view,
+    function(x, ...) {
+      purrr::modify_at(
+        x, "data",
+        function(xdata, ...) dplyr::select(xdata, ...),
+        ...
+      )
+    }, ...
+  )
+}
