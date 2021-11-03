cfd8b5a3fb720b3c5a13f5a128c874ff24f4ea5c
schae211 <schae211@msu.edu>
2021-11-03 11:18:36 +0100

    building the model functions for the new api

--- R/functions.R
+++ R/functions.R
@@ -131,7 +131,7 @@ test$importances
 
 #####
 
-bagged_linear_model = function(view_data, target, seed, n.vars...) {
+bagged_linear_model = function(view_data, target, seed, n.vars, ...) {
   
   ellipsis.args <- list(...)
   
@@ -201,4 +201,240 @@ test$unbiased.predictions
 test$importances
 
 #####
+
+cv_linear_model = function(view_data, target, seed, ...) {
+
+  ellipsis.args <- list(...)
+  
+  folds <- withr::with_seed(
+    seed,
+    caret::createFolds(seq.int(1, nrow(view_data)), k = 10)
+  )
+  
+  holdout.predictions <- purrr::map_dfr(folds, function(holdout) {
+    
+    in.fold <- seq.int(1, nrow(view_data))[!(seq.int(1, nrow(view_data)) %in% holdout)]
+    
+    algo.arguments <- list(
+      formula = stats::as.formula(paste0(target, " ~ .")),
+      data = view_data[in.fold, ]
+    )
+    
+    if (!(length(ellipsis.args) == 0)) {
+      algo.arguments <- merge_2(algo.arguments, ellipsis.args)
+    }
+    
+    model <- do.call(stats::lm, algo.arguments)
+    
+    pred <- predict.lm(model, view_data[holdout, ])
+    
+    tibble::tibble(index = holdout, prediction = as.vector(pred))
+  }) %>% dplyr::arrange(index)
+  
+  algo.arguments.wm <- list(
+    formula = stats::as.formula(paste0(target, " ~ .")),
+    data = view_data
+  )
+  
+  if (!(length(ellipsis.args) == 0)) {
+    algo.arguments.wm <- merge_2(algo.arguments.wm, ellipsis.args)
+  }
+  
+  whole.model <- do.call(stats::lm, algo.arguments.wm)
+  
+  importances <- whole.model$coefficients[names(whole.model$coefficients) != "(Intercept)"]
+  
+  list(unbiased.predictions = holdout.predictions, 
+       importances = importances)
+}
+
+#####
+
+expr <- synthetic$synthetic1 %>% select(-c(row, col, type))
+test <- cv_linear_model(expr, "ECM", 42)
+test$unbiased.predictions
+test$importances
+
+#####
+
+cv_svm_model = function(view_data, target, seed, ...) {
+  
+  ellipsis.args <- list(...)
+  
+  folds <- withr::with_seed(
+    seed,
+    caret::createFolds(seq.int(1, nrow(view_data)), k = 10)
+  )
+  
+  holdout.predictions <- purrr::map_dfr(folds, function(holdout) {
+    
+    in.fold <- seq.int(1, nrow(view_data))[!(seq.int(1, nrow(view_data)) %in% holdout)]
+    
+    algo.arguments <- list(
+      x = stats::as.formula(paste0(target, " ~ .")),
+      data = view_data[in.fold, ],
+      kernel = "vanilladot",
+      C = 1,
+      type = "eps-svr",
+      kpar = list() # no hyperparameters for linear kernel
+    )
+    
+    if (!(length(ellipsis.args) == 0)) {
+      algo.arguments <- merge_2(algo.arguments, ellipsis.args)
+    }
+    
+    model <- do.call(kernlab::ksvm, algo.arguments)
+    
+    pred <- kernlab::predict(model, view_data[holdout, ])
+    
+    tibble::tibble(index = holdout, prediction = as.vector(pred))
+  }) %>% dplyr::arrange(index)
+  
+  algo.arguments.wm <- list(
+    x = stats::as.formula(paste0(target, " ~ .")),
+    data = view_data,
+    kernel = "vanilladot",
+    C = 1,
+    type = "eps-svr",
+    kpar = list() # no hyperparameters for linear kernel
+  )
+  
+  if (!(length(ellipsis.args) == 0)) {
+    algo.arguments.wm <- merge_2(algo.arguments.wm, ellipsis.args)
+  }
+  
+  whole.model <- do.call(kernlab::ksvm, algo.arguments.wm)
+  
+  importances <- (t(whole.model@coef) %*% whole.model@xmatrix)[1,]
+  
+  list(unbiased.predictions = holdout.predictions, 
+       importances = importances)
+}
+
+#####
+
+expr <- synthetic$synthetic1 %>% select(-c(row, col, type))
+test <- cv_svm_model(expr, "ECM", 42)
+test$unbiased.predictions
+test$importances
+
+#####
+
+cv_boosted_trees_model = function(view_data, target, seed, ...) {
+  
+  ellipsis.args <- list(...)
+  
+  folds <- withr::with_seed(
+    seed,
+    caret::createFolds(seq.int(1, nrow(view_data)), k = 10)
+  )
+  
+  holdout.predictions <- purrr::map_dfr(folds, function(holdout) {
+    
+    in.fold <- seq.int(1, nrow(view_data))[!(seq.int(1, nrow(view_data)) %in% holdout)]
+    
+    train <- expr[in.fold, ]
+    test <- expr[holdout, ]
+    
+    pred.train <- train %>% select(-all_of(target)) %>% as.matrix
+    label.train <- train %>% pull(all_of(target))
+    
+    algo.arguments <- list(
+      params = list(booster = "gbtree", eta = 0.3, objective = "reg:squarederror",
+                    max.depth = 6, nthread = 1),
+      data = pred.train,
+      label = label.train,
+      nrounds = 10,
+      verbose = 0
+    )
+    
+    if (!(length(ellipsis.args) == 0)) {
+      algo.arguments <- merge_2(algo.arguments, ellipsis.args)
+    }
+    
+    model <- do.call(xgboost::xgboost, algo.arguments)
+
+    pred.test <- test %>% select(-all_of(target)) %>% as.matrix
+    
+    label.hat <- predict(model, pred.test)
+    
+    tibble::tibble(index = holdout, prediction = label.hat)
+  }) %>% dplyr::arrange(index)
+  
+
+  
+  whole.model <- do.call(kernlab::ksvm, algo.arguments.wm)
+  
+  importances <- (t(whole.model@coef) %*% whole.model@xmatrix)[1,]
+  
+  list(unbiased.predictions = holdout.predictions, 
+       importances = importances)
+}
+
+#####
+
+# Learning about xgboost
+
+predictors <- expr %>% select(-ECM) %>% as.matrix
+labels <- expr %>% pull(ECM)
+
+test <- xgboost::xgboost(
+  params = list(booster = "gbtree", eta = 0.3, objective = "reg:squarederror",
+                max.depth = 2, nthread = 1),
+  data = predictors,
+  label = labels,
+  nrounds = 10
+)
+
+pred <- predict(test, predictors)
+
+importance_matrix <- xgboost::xgb.importance(model = test)
+importance_matrix
+
+importance <- unlist(importance_matrix[, "Gain"])
+names(importance) <- unlist(importance_matrix[, "Feature"])
+importance
+
+#####
+
+# Hyperparameter Tuning
+
+folds <- withr::with_seed(
+  seed,
+  caret::createFolds(seq.int(1, nrow(view_data)), k = 10)
+)
+
+hyper.test <- imap_dfr(folds, function(holdout, i) {
+  
+  in.fold <- seq.int(1, nrow(view_data))[!(seq.int(1, nrow(view_data)) %in% holdout)]
+  
+  train <- expr[in.fold, ]
+  test <- expr[holdout, ]
+  
+  pred.train <- train %>% select(-ECM) %>% as.matrix
+  label.train <- train %>% pull(ECM)
+  
+  model <- xgboost::xgboost(
+    params = list(booster = "gbtree", eta = 0.3, objective = "reg:squarederror",
+                  max.depth = 6, nthread = 1),
+    data = pred.train,
+    label = label.train,
+    nrounds = 10,
+    verbose = 0
+  )
+  
+  pred.test <- test %>% select(-ECM) %>% as.matrix
+  label.test <- test %>% pull(ECM)
+  label.hat <- predict(model, pred.test)
+  
+  RMSE <- caret::RMSE(label.hat, label.test)
+  R2 <- caret::R2(label.hat, label.test)
+  
+  tibble::tibble(cv = i, r2 = R2, rmse = RMSE)
+  
+}) %>%
+  rbind(., tibble::tibble(cv = "Summary", r2 = mean(.$r2), rmse = mean(.$rmse))) 
+
+hyper.test
+
 
